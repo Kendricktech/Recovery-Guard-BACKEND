4344a922279e12f397810f601c6e39afd75b450d
kode <olorik362@gmail.com>
2025-04-26 21:32:07 +0100

    update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,10 +2,27 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-#@admin.register(CustomUser)
-#class CustomUserAdmin(UserAdmin):
-#    search_fields = ['email', 'first_name', 'last_name']  # or however your model is defined
-    #list_display = ['email', 'is_agent', 'is_customer']
-    #exclude =['date_joined',]
+class CustomUserAdmin(UserAdmin):
+    # The forms to add and change user instances
+    list_display = ('email', 'first_name', 'last_name', 'username', 'is_staff', 
+                    'is_active', 'is_agent', 'is_customer')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_agent', 'is_customer')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'username')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 
+                                   'groups', 'user_permissions')}),
+        ('Roles', {'fields': ('is_agent', 'is_customer')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
 
-admin.site.register(CustomUser)
+# Register the admin class
+admin.site.register(CustomUser, CustomUserAdmin)
