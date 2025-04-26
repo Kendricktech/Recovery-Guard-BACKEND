bcf0402ff73ed03b567f1ed5dc59c2b2ac0abf1d
kode <olorik362@gmail.com>
2025-04-26 21:26:13 +0100

    update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    search_fields = ['email', 'first_name', 'last_name']  # or however your model is defined
-    list_display = ['email', 'is_agent', 'is_customer']
-    exclude =['date_joined',]
+#@admin.register(CustomUser)
+#class CustomUserAdmin(UserAdmin):
+#    search_fields = ['email', 'first_name', 'last_name']  # or however your model is defined
+    #list_display = ['email', 'is_agent', 'is_customer']
+    #exclude =['date_joined',]
+
+admin.site.register(CustomUser)
