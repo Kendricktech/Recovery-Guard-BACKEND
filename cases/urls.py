56c71271047a2f23129b83c244a787306c5b0c26
kode <olorik362@gmail.com>
2025-04-26 21:51:07 +0100

    update urls.py

--- cases/urls.py
+++ cases/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CaseListApiView,CaseDetailView
+from .views import CaseListApiView,CaseDetailView,CreateCryptoLossAPIView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('cases/',CaseListApiView.as_view(),name='case-list'),
     path('<int:pk>/',CaseDetailView.as_view(),name='case-detail'),
+    
+    path('crypto-loss',CreateCryptoLossAPIView.as_view(), name='crypto-loss'),
     
 ]
