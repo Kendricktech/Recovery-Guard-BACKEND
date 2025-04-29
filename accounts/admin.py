fe17c53476606c057fb84d75583dacb20005b84b
Kode <olorik362@gmail.com>
2025-04-29 16:37:57 +0100

    Fixed the admin class for CustomUser Admin Panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,5 +4,17 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    search_fields = ['email', 'first_name', 'last_name']  # or however your model is defined
-    list_display = ['email', 'is_agent', 'is_customer']
+    search_fields = ['email', 'first_name', 'last_name']
+    list_display = ['email', 'is_agent', 'is_customer', 'is_active', 'is_staff']
+    list_filter = ['is_agent', 'is_customer', 'is_active', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_agent', 'is_customer')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_agent', 'is_customer')}),
+    )
